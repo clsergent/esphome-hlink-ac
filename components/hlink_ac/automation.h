2f426945e8ea12928c7f28cf58e442084394d280
clsergent <sergent.cl@laposte.net>
2025-06-18 12:49:35 +0200

    update

--- components/hlink_ac/automation.h
+++ components/hlink_ac/automation.h
@@ -16,5 +16,16 @@ template<typename... Ts> class HlinkAcSendHlinkCmd : public Action<Ts...>, publi
     this->parent_->send_hlink_cmd(address, data);
   }
 };
+template<typename... Ts> class HlinkAcSendHlinkRequest : public Action<Ts...>, public Parented<HlinkAc> {
+ public:
+  TEMPLATABLE_VALUE(std::string, address)
+  TEMPLATABLE_VALUE(std::string, data)
+
+  void play(Ts... x) override {
+    auto address = this->address_.value(x...);
+    auto data = this->data_.value(x...);
+    this->parent_->send_hlink_request(address, data);
+  }
+};
 }  // namespace hlink_ac
 }  // namespace esphome
