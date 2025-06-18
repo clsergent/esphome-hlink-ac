8bb458e8fd4e1c48f2c386af308d1f8a4612a428
clsergent <sergent.cl@laposte.net>
2025-06-18 19:51:37 +0200

    update

--- components/hlink_ac/hlink_ac.cpp
+++ components/hlink_ac/hlink_ac.cpp
@@ -460,7 +460,7 @@ HlinkResponseFrame HlinkAc::read_hlink_frame_(uint32_t timeout_ms) {
       }
       read_index++;
     }
-    ESP_LOGD(TAG, "RECEIVED: L=%d, S=%.*s", read_index, read_index, response_buf.c_str());
+    //ESP_LOGD(TAG, "RECEIVED: L=%d, S=%.*s", read_index, read_index, response_buf.c_str());
 
     // Update the timestamp of the last frame received
     this->status_.last_frame_received_at_ms = millis();
